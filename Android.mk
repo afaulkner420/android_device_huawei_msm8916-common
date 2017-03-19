14c90423ef3d86d9bcafb70302d17498976f4a9b
Mustang-ssc <lilhali6@gmail.com>
2017-03-19 13:21:24 +0100

    Update Android.mk
    Updated Android.mk for Mediapad T1 10 device.

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter cherry che10,$(TARGET_DEVICE)),)
+ifneq ($(filter hwt1a21l,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
